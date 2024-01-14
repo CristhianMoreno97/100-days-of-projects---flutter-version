37a0c6ae7ff8c42d88da9e68d55cdb6a9b2288a8
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-01-14 10:34:06 -0500

    add new card components

--- lib/features/challengues/presentation/screens/day_2_screen.dart
+++ lib/features/challengues/presentation/screens/day_2_screen.dart
@@ -22,13 +22,13 @@ class _MainView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(40),
-      child: const Center(child: _BlogCard()),
+      child: const Center(child: _Card()),
     );
   }
 }
 
-class _BlogCard extends StatelessWidget {
-  const _BlogCard();
+class _Card extends StatelessWidget {
+  const _Card();
 
   @override
   Widget build(BuildContext context) {
@@ -57,11 +57,35 @@ class _BlogCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _CardImage(),
-          SizedBox(height: 8),
+          SizedBox(height: 16),
+          _CardTag(),
+          SizedBox(height: 4),
           _CardPublishedDate(),
+          SizedBox(height: 16 ),
+          _CardTitle(),
+          SizedBox(height: 8),
+          _CardCopy(),
+          SizedBox(height: 16),
+          _CardFooter(),
         ],
       ),
     );
+  }
+}
+
+class _CardTag extends StatelessWidget {
+  const _CardTag();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        decoration: BoxDecoration(
+          color: const Color(0xFFF5D04E),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: const Text('Learning',
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600)));
   }
 }
 
@@ -70,9 +94,25 @@ class _CardPublishedDate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = DateTime.parse('2023-01-13');
+    final dateTime = DateTime.parse('2024-01-14');
     final formattedDate = DateFormat.yMMMMd().format(dateTime);
-    return Text('Published on $formattedDate');
+    return Text('Published on $formattedDate',
+        style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w600));
+  }
+}
+
+class _CardTitle extends StatelessWidget {
+  const _CardTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'HTML & CSS foundations',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+      ),
+    );
   }
 }
 
@@ -89,6 +129,62 @@ class _CardImage extends StatelessWidget {
           'assets/images/day_2_illustration-article.svg',
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _CardCopy extends StatelessWidget {
+  const _CardCopy();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'These languages are backbone of every website, defining structure, content, and presentation.',
+      style: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: Colors.grey,
+      ),
+    );
+  }
+}
+
+class _CardFooter extends StatelessWidget {
+  const _CardFooter();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(children: [
+      _UserAvatar(),
+      SizedBox(width: 8),
+      _UserName(),
+    ]);
+  }
+}
+
+class _UserAvatar extends StatelessWidget {
+  const _UserAvatar();
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(
+      radius: 15,
+      backgroundImage: AssetImage('assets/images/day_1_card_image.png'),
+    );
+  }
+}
+
+class _UserName extends StatelessWidget {
+  const _UserName();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Cristhian Moreno',
+      style: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w600,
       ),
     );
   }
